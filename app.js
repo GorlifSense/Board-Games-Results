ab4d27cb76548488275fb48016b32be74730d3ee
Fleischers <Fleischer458@gmail.com>
2016-06-11 14:43:06 +0300

    Added serving of static files

---
+++ app.js
@@ -0,0 +1,35 @@
+var koa = require('koa');
+var app = koa();
+var winston = require('winston');
+var favicon = require('koa-favicon');
+var serve = require('koa-static');
+
+// logger
+var LOG_LEVEL = 'debug';
+// remove old transport
+winston.remove(winston.transports.Console);
+// add new with Console-logger settings
+winston.add(winston.transports.Console, {
+    colorize: true,
+    level: LOG_LEVEL,
+    prettyPrint: true,
+    humanReadableUnhandledException: true
+});
+
+app.use(function *(next){
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  winston.debug('%s %s - %s', this.method, this.url, ms);
+});
+
+// response
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(serve('public'));
+
+//
+// app.use(function *(){
+//   this.body = 'Hello World';
+// });
+
+app.listen(3000);
