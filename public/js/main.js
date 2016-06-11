3af560ec56c89820bc5c1eb5c730ed25ecce6837
Belrestro <belrestro@gmail.com>
2016-06-11 18:09:14 +0300

    Added basic new table template without sending function

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,39 @@
-console.log("Hello");
+(function(){
+	document.addEventListener('DOMContentLoaded', function(){
+		var getE = function(query, parent){
+			var selector = query.charAt(0);
+			var name = query.slice(1);
+			var element = parent || document;
+			if(selector == '#'){
+				return document.getElementById(name);
+			} else if(selector == '.'){
+				return element.getElementsByClassName(name);
+			} else {
+				return element.getElementsByTagName(query);
+			}
+		};
+		var playerForm = getE('.player-none')[0];
+		var addPlayer = getE('#add-player');
+		addPlayer.addEventListener('click', function(){
+			var players = getE('.player');
+			var newPlayer = playerForm.cloneNode(true);
+			var removeButton = document.createElement('button');
+
+			newPlayer.className = 'player';
+
+			removeButton.className = "remove-player btn btn-danger";
+			removeButton.innerHTML = "-";
+			removeButton.addEventListener('click', function(){
+				newPlayer.style.animationName = 'playerFade';
+				newPlayer.style.animationDuration = '.5s';
+				var x = setTimeout(function(){
+					players[0].parentNode.removeChild(newPlayer);
+				}, 500);
+			});
+			newPlayer.appendChild(removeButton);
+
+			var controls = getE('.controls')[0];
+			players[0].parentNode.insertBefore(newPlayer, controls);
+		});
+	});
+})();
